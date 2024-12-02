48bd38a46a80982971d37c41da3bb1847446ef9a
Jorge Nunes <stamina.nunes@gmail.com>
2024-12-02 18:03:27 +0000

    Initial commit, sensors should work.

---
+++ main.cpp
@@ -0,0 +1,92 @@
+//
+//  main.cpp
+//  ps5-kontroller-sdl2-senson
+//
+//  Created by Jorge Domingues Nunes on 02/12/2024.
+//
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_GAMECONTROLLER | SDL_INIT_SENSOR) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Open the first available controller
+    SDL_GameController *controller = NULL;
+    for (int i = 0; i < SDL_NumJoysticks(); ++i) {
+        if (SDL_IsGameController(i)) {
+            controller = SDL_GameControllerOpen(i);
+            if (controller) {
+                printf("Controller opened: %s\n", SDL_GameControllerName(controller));
+                break;
+            } else {
+                printf("Could not open controller: %s\n", SDL_GetError());
+            }
+        }
+    }
+
+    if (controller == NULL) {
+        printf("No controller detected!\n");
+        SDL_Quit();
+        return 1;
+    }
+
+    // Enable sensors (Accelerometer and Gyroscope)
+    if (SDL_GameControllerHasSensor(controller, SDL_SENSOR_ACCEL)) {
+        if (SDL_GameControllerSetSensorEnabled(controller, SDL_SENSOR_ACCEL, SDL_TRUE) < 0) {
+            printf("Failed to enable accelerometer: %s\n", SDL_GetError());
+        } else {
+            printf("Accelerometer enabled.\n");
+        }
+    }
+
+    if (SDL_GameControllerHasSensor(controller, SDL_SENSOR_GYRO)) {
+        if (SDL_GameControllerSetSensorEnabled(controller, SDL_SENSOR_GYRO, SDL_TRUE) < 0) {
+            printf("Failed to enable gyroscope: %s\n", SDL_GetError());
+        } else {
+            printf("Gyroscope enabled.\n");
+        }
+    }
+
+    SDL_Event event;
+    int running = 1;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+
+            // Handle device removal
+            if (event.type == SDL_CONTROLLERDEVICEREMOVED) {
+                printf("Controller removed!\n");
+                running = 0;
+            }
+        }
+
+        // Check for accelerometer data
+        float accel[3];
+        if (SDL_GameControllerGetSensorData(controller, SDL_SENSOR_ACCEL, accel, 3) == 0) {
+            printf("Accelerometer: X=%f, Y=%f, Z=%f\n", accel[0], accel[1], accel[2]);
+        }
+
+        // Check for gyroscope data
+        float gyro[3];
+        if (SDL_GameControllerGetSensorData(controller, SDL_SENSOR_GYRO, gyro, 3) == 0) {
+            printf("Gyroscope: X=%f, Y=%f, Z=%f\n", gyro[0], gyro[1], gyro[2]);
+        }
+
+        SDL_Delay(100); // Avoid spamming output
+    }
+
+    // Clean up
+    SDL_GameControllerClose(controller);
+    SDL_Quit();
+
+    return 0;
+}
